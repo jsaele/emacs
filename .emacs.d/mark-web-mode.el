37c7a1774315b1a3bd7eb78e4eebc627fb02eb24
Joakim Saelemyr <joakim.saelemyr@gmail.com>
2014-12-21 13:01:43 +0100

    autopair

--- .emacs.d/mark-web-mode.el
+++ .emacs.d/mark-web-mode.el
@@ -1,22 +1,23 @@
 
 (package-require 'web-mode)
+(package-require 'autopair)
 
 ;;(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
 
 (defun my-web-mode-hook ()
   (setq web-mode-markup-indent-offset 4)
   (setq web-mode-css-indent-offset 4)
   (setq web-mode-code-indent-offset 4)
+;;  (setq web-mode-enable-auto-pairing t)
+;;  (setq web-mode-extra-auto-pairs
+;;      '(("js"  . (("{" "}")))
+;;        ("php"  . (("open" "close")
+;;                   ("open" "close")))
+;;        ))
 )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
@@ -24,12 +25,8 @@
 (add-to-list 'load-path "~/system/tern/emacs/")
 (autoload 'tern-mode "tern" nil t)
 (add-hook 'web-mode-hook (lambda () (tern-mode t)))
+(add-hook 'web-mode-hook (lambda () (autopair-mode t)))
 (add-hook 'web-mode-hook '(lambda() (setq indent-tabs-mode nil)))
 (add-hook 'web-mode-hook (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
-(add-hook 'web-mode-hook (lambda () (setq web-mode-enable-auto-pairing t)))
-(add-hook 'web-mode-hook (lambda () (setq web-mode-extra-auto-pairs
-      '(("js"  . (("{" "}")))
-        ("php"  . (("open" "close")
-                   ("open" "close")))
-       ))))
+
 (provide 'mark-web-mode)
